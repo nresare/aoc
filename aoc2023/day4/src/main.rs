cf8adf2d6dc924f771fd69a5ae483fdb7c341a0d
Noa Resare <noa@resare.com>
2023-12-31 11:33:38 +0000

    cargo fmt

--- aoc2023/day4/src/main.rs
+++ aoc2023/day4/src/main.rs
@@ -1,5 +1,5 @@
-use std::collections::{HashMap, HashSet};
 use anyhow::{anyhow, Result};
+use std::collections::{HashMap, HashSet};
 
 #[derive(PartialEq, Debug)]
 struct Card {
@@ -11,18 +11,22 @@ impl Card {
     fn parse(input: &str) -> Result<Self> {
         let parts: Vec<&str> = input.split(": ").collect();
         if parts.len() != 2 {
-            return Err(anyhow!("Invalid format"))
+            return Err(anyhow!("Invalid format"));
         }
         let input = parts[1];
         let parts: Vec<_> = input.split(" | ").collect();
         if parts.len() != 2 {
-            return Err(anyhow!("Invalid format"))
+            return Err(anyhow!("Invalid format"));
         }
-        let winners: HashSet<i32> = parts[0].split_whitespace().map(
-            |s| s.parse().unwrap()
-        ).collect();
-        let numbers: Vec<i32> = parts[1].split_whitespace().map(|s| s.parse().unwrap()).collect();
-        Ok(Card{winners, numbers})
+        let winners: HashSet<i32> = parts[0]
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        let numbers: Vec<i32> = parts[1]
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        Ok(Card { winners, numbers })
     }
 
     fn points(&self) -> i32 {
@@ -44,7 +48,10 @@ impl Card {
 }
 
 fn main() -> Result<()> {
-    let cards: Vec<Card> = include_str!("input.txt").lines().map(Card::parse).collect::<Result<_>>()?;
+    let cards: Vec<Card> = include_str!("input.txt")
+        .lines()
+        .map(Card::parse)
+        .collect::<Result<_>>()?;
     println!("part 1: {}", cards.iter().map(Card::points).sum::<i32>());
     let mut m: HashMap<i32, i32> = HashMap::new();
 
@@ -63,28 +70,31 @@ fn main() -> Result<()> {
 }
 
 fn increment(m: &mut HashMap<i32, i32>, id: i32, increment: i32) {
-    m.entry(id).and_modify(|i| *i += increment).or_insert(increment);
+    m.entry(id)
+        .and_modify(|i| *i += increment)
+        .or_insert(increment);
 }
 
 fn card_id(t: (usize, &Card)) -> (i32, &Card) {
     ((t.0 + 1) as i32, t.1)
 }
 
-
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
     use crate::Card;
+    use std::collections::HashMap;
 
     #[test]
     fn test_parse() {
         let card = Card::parse("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53").unwrap();
-        assert_eq!(Card{
-            winners: [41, 48, 83, 86, 17].into_iter().collect(),
-            numbers: vec![83, 86, 6, 31, 17, 9, 48, 53]
-        }, card);
+        assert_eq!(
+            Card {
+                winners: [41, 48, 83, 86, 17].into_iter().collect(),
+                numbers: vec![83, 86, 6, 31, 17, 9, 48, 53]
+            },
+            card
+        );
     }
-
 
     #[test]
     fn test_counter() {
@@ -92,6 +102,6 @@ mod tests {
         map.entry(42).and_modify(|x| *x += 1).or_insert(1);
         map.entry(42).and_modify(|x| *x += 1).or_insert(1);
         map.entry(47).and_modify(|x| *x += 1).or_insert(1);
-        assert_eq!(HashMap::from([(42,2), (47,1)]), map);
+        assert_eq!(HashMap::from([(42, 2), (47, 1)]), map);
     }
 }
