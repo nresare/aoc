a612f7b672fb30e4498a7b979d163d8704d4dba6
Noa Resare <noa@resare.com>
2024-01-07 13:16:19 +0000

    day 7 cleanup

--- aoc2023/day7/src/main.rs
+++ aoc2023/day7/src/main.rs
@@ -1,19 +1,20 @@
-
-use std::cmp::Ordering;
-use std::fmt::{Debug, Formatter};
+use crate::HandType::{FullHouse, OnePair, ThreeOfAKind, TwoPair};
 use anyhow::anyhow;
 use counter::Counter;
+use std::cmp::Ordering;
+use std::fmt::{Debug, Formatter};
 
 fn main() {
     let mut v = parse(include_str!("input.txt"));
-    v.sort();
+    v.sort_by(part_1_compare);
     println!("part 1: {}", count(&v));
     v.sort_by(part_2_compare);
     println!("part 2: {}", count(&v));
 }
 
-fn count(sorted_games: &Vec<(Hand, u32)>) -> u32 {
-    sorted_games.iter()
+fn count(sorted_games: &[(Hand, u32)]) -> u32 {
+    sorted_games
+        .iter()
         .enumerate()
         .map(|(i, (_, bet))| bet * (i + 1) as u32)
         .sum()
@@ -30,22 +31,34 @@ fn part_2_compare(a: &(Hand, u32), b: &(Hand, u32)) -> Ordering {
     }
 }
 
+fn part_1_compare(a: &(Hand, u32), b: &(Hand, u32)) -> Ordering {
+    let this_type = a.0.best_type();
+    let other_type = b.0.best_type();
+    match other_type.cmp(&this_type) {
+        Ordering::Equal => compare_by_card_value(&b.0, &a.0, ORDER),
+        ordering => ordering,
+    }
+}
+
 fn convert_jokers(hand: &Hand) -> Hand {
-    let freqs = hand.values.into_iter().map(|i| i as char).collect::<Counter<_>>();
-    if ! freqs.contains_key(&'J') || freqs[&'J'] == 5 {
+    let freqs = hand
+        .values
+        .into_iter()
+        .map(|i| i as char)
+        .collect::<Counter<_>>();
+    if !freqs.contains_key(&'J') || freqs[&'J'] == 5 {
         // no jokers to convert
-        return hand.clone()
+        return hand.clone();
     }
 
     let by_order = freqs.most_common_ordered();
-    let convert_to;
-    if by_order[0].0 == 'J' {
+    let convert_to = if by_order[0].0 == 'J' {
         // jokers are the most common. Turn the jokers into the next most common
-        convert_to = by_order[1].0
+        by_order[1].0
     } else {
         // jokers are not the most common, turn them into the most common
-        convert_to = by_order[0].0
-    }
+        by_order[0].0
+    };
     let s = &String::from_utf8_lossy(&hand.values[..])[..];
     let result = s.replace('J', &convert_to.to_string()[..]);
     Hand::new(&result[..]).expect("Failed to construct hand")
@@ -55,49 +68,47 @@ fn best_type_with_joker(hand: &Hand) -> HandType {
     let hand = convert_jokers(hand);
 
     let counts = hand.values.into_iter().collect::<Counter<_>>();
-    let counts = counts.values().map(|x| *x).collect::<Vec<_>>();
+    let counts = counts.values().copied().collect::<Vec<_>>();
     counts_to_hand_type(counts)
 }
 
-fn counts_to_hand_type(counts: Vec<usize>) -> HandType {
-    if counts.contains(&5) {
-        HandType::FiveOfAKind
-    } else if counts.contains(&4) {
-        HandType::FourOfAKind
-    } else if counts.contains(&3) {
-        if counts.contains(&2) {
-            HandType::FullHouse
-        } else {
-            HandType::ThreeOfAKind
-        }
-    } else if counts.contains(&2) {
-        let mut pair_count = 0;
-        for c in counts {
-            if c == 2 {
-                pair_count += 1;
+fn counts_to_hand_type(mut counts: Vec<usize>) -> HandType {
+    counts.sort();
+    match counts[counts.len() - 1] {
+        5 => HandType::FiveOfAKind,
+        4 => HandType::FourOfAKind,
+        3 => {
+            if counts.contains(&2) {
+                FullHouse
+            } else {
+                ThreeOfAKind
             }
         }
-        if pair_count == 2 {
-            HandType::TwoPair
-        } else {
-            HandType::OnePair
+        2 => {
+            if counts[counts.len() - 2] == 2 {
+                TwoPair
+            } else {
+                OnePair
+            }
         }
-    } else {
-        HandType::HighCard
+        _ => HandType::HighCard,
     }
-
 }
 
 fn parse(input: &str) -> Vec<(Hand, u32)> {
-    input.lines().map(|l| {
-        let mut parts = l.split_whitespace();
-        (Hand::new(parts.next().unwrap()).unwrap(), parts.next().unwrap().parse::<u32>().unwrap())
-    }).collect()
+    input
+        .lines()
+        .map(|l| {
+            let mut parts = l.split_whitespace();
+            (
+                Hand::new(parts.next().unwrap()).unwrap(),
+                parts.next().unwrap().parse::<u32>().unwrap(),
+            )
+        })
+        .collect()
 }
 
-
 const ORDER: &[u8] = "AKQJT98765432".as_bytes();
-
 
 #[derive(PartialOrd, PartialEq, Ord, Eq, Debug)]
 pub enum HandType {
@@ -107,11 +118,10 @@ pub enum HandType {
     ThreeOfAKind,
     TwoPair,
     OnePair,
-    HighCard
+    HighCard,
 }
 
-
-#[derive(Eq, Clone)]
+#[derive(Clone, PartialEq)]
 pub struct Hand {
     pub(crate) values: [u8; 5],
 }
@@ -119,19 +129,24 @@ pub struct Hand {
 impl Hand {
     pub fn new(data: &str) -> anyhow::Result<Self> {
         if data.len() != 5 {
-            return Err(anyhow!("Wrong length of data: {}", data.len()))
+            return Err(anyhow!("Wrong length of data: {}", data.len()));
         }
         for c in data.as_bytes() {
-            if ! ORDER.contains(c) {
-                return Err(anyhow!("Invalid character {}", *c as char))
+            if !ORDER.contains(c) {
+                return Err(anyhow!("Invalid character {}", *c as char));
             }
         }
-        Ok(Hand {values: data.as_bytes().try_into().expect("input has incorrect length")})
+        Ok(Hand {
+            values: data
+                .as_bytes()
+                .try_into()
+                .expect("input has incorrect length"),
+        })
     }
 
     pub(crate) fn best_type(&self) -> HandType {
         let counts = self.values.into_iter().collect::<Counter<_>>();
-        let counts = counts.values().map(|x| *x).collect::<Vec<_>>();
+        let counts = counts.values().copied().collect::<Vec<_>>();
         counts_to_hand_type(counts)
     }
 }
@@ -144,38 +159,16 @@ impl Debug for Hand {
     }
 }
 
-impl PartialEq<Self> for Hand {
-    fn eq(&self, other: &Self) -> bool {
-        self.values == other.values
-    }
-}
-
-
-
-impl PartialOrd<Self> for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let this_type = self.best_type();
-        let other_type = other.best_type();
-        match other_type.cmp(&this_type) {
-            Ordering::Equal => compare_by_card_value(other, self, ORDER),
-            ordering => ordering,
-        }
-    }
-}
-
 fn get_card_value(card: u8, positions: &[u8]) -> u32 {
-    positions.iter().position(|i| *i == card).expect("invalid card") as u32
+    positions
+        .iter()
+        .position(|i| *i == card)
+        .expect("invalid card") as u32
 }
-pub(crate) fn compare_by_card_value(a: &Hand, b: &Hand, positions: &[u8]) -> Ordering{
-    let mut a = a.values.into_iter().map(|x| get_card_value(x, positions));
+pub(crate) fn compare_by_card_value(a: &Hand, b: &Hand, positions: &[u8]) -> Ordering {
+    let a = a.values.into_iter().map(|x| get_card_value(x, positions));
     let mut b = b.values.into_iter().map(|x| get_card_value(x, positions));
-    while let Some(a_value) = a.next() {
+    for a_value in a {
         let b_value = b.next().expect("a and be not equally long");
         match a_value.cmp(&b_value) {
             Ordering::Greater => return Ordering::Greater,
@@ -186,16 +179,13 @@ pub(crate) fn compare_by_card_value(a: &Hand, b: &Hand, positions: &[u8]) -> Ord
     Ordering::Equal
 }
 
-
-
-
-    #[cfg(test)]
+#[cfg(test)]
 mod tests {
-    use std::cmp::Ordering;
-    use crate::convert_jokers;
-    use crate::HandType;
+    use crate::{convert_jokers, part_1_compare};
     use crate::Hand;
+    use crate::HandType;
     use crate::HandType::*;
+    use std::cmp::Ordering;
 
     #[test]
     fn test_new_hand() {
@@ -218,7 +208,6 @@ mod tests {
         best_kind("23456", HighCard);
     }
 
-
     fn best_kind(input: &str, t: HandType) {
         let hand = Hand::new(input).unwrap();
         assert_eq!(t, hand.best_type())
@@ -235,27 +224,11 @@ mod tests {
     }
 
     fn cmp(first: &str, second: &str, expected: Ordering) {
-        assert_eq!(expected, Hand::new(second).unwrap().cmp(&Hand::new(first).unwrap()))
-    }
-
-    #[test]
-    fn test_sort() {
-        let mut v = vec![
-            Hand::new("AAAAA").unwrap(),
-            Hand::new("KK677").unwrap(),
-            Hand::new("KTJJT").unwrap(),
-        ];
-        v.sort();
         assert_eq!(
-            vec![
-                Hand::new("KTJJT").unwrap(),
-                Hand::new("KK677").unwrap(),
-                Hand::new("AAAAA").unwrap(),
-            ],
-            v
-        );
+            expected,
+            part_1_compare(&(Hand::new(first).unwrap(),0), &(Hand::new(second).unwrap(),0)),
+        )
     }
-
 
     #[test]
     fn test_convert_jokers() {
@@ -267,6 +240,5 @@ mod tests {
     fn cj(from: &str, to: &str) {
         let result = convert_jokers(&Hand::new(from).unwrap());
         assert_eq!(Hand::new(to).unwrap(), result);
-
     }
 }
