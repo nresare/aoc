7d6637cb179e0b8a6e167ab4786e1999b37c2e82
Noa Resare <noa@resare.com>
2024-02-22 16:24:09 +0000

    WIP: 2023 day 8, part 2

--- aoc2023/day8/src/main.rs
+++ aoc2023/day8/src/main.rs
@@ -13,6 +13,12 @@ lazy_static! {
 
 fn main() -> Result<()> {
     let data = parse(include_str!("input.txt"))?;
+    part_1(&data);
+    part_2(&data);
+    Ok(())
+}
+
+fn part_1(data: &Data) {
     let mut count = 0;
     let mut name = START;
     while name != END {
@@ -20,7 +26,42 @@ fn main() -> Result<()> {
         name = follow_instructions(&data, name)
     }
     println!("Found end after {count} rounds");
-    Ok(())
+}
+
+fn part_2(data: &Data) {
+    let mut count = 0;
+    let mut names = find_ends_with_a(&data.network);
+    let mut cache: HashMap<&str, &str> = HashMap::new();
+
+    loop {
+        count += 1;
+        let mut all_at_end = true;
+        let mut success_count = 0;
+        for name in names.iter_mut() {
+            let result= cache.entry(name).or_insert_with(|| follow_instructions(&data, *name));
+            if !result.ends_with("Z") {
+                all_at_end = false;
+            } else {
+                success_count += 1;
+            }
+            *name = result;
+        }
+        if all_at_end {
+            break;
+        }
+        if success_count > 2 {
+            println!("success count at {}: {}", count, success_count);
+        }
+        if count % 1_000_000 == 0 {
+            println!("Checked {} sets. Success count: {}", count, success_count)
+        }
+    }
+    println!("Found end after {} rounds", count * data.instructions.len());
+
+}
+
+fn find_ends_with_a(network: &Network) -> Vec<&str> {
+    network.keys().filter(|k| k.ends_with("A")).map(String::as_str).collect()
 }
 
 fn follow_instructions<'a>(data: &'a Data, mut start: &'a str) -> &'a str {
@@ -31,7 +72,7 @@ fn follow_instructions<'a>(data: &'a Data, mut start: &'a str) -> &'a str {
 }
 
 fn next<'a>(network: &'a Network, name: &'a str, direction: &Direction) -> &'a str {
-    let pair = network.get(name).expect("Could not find pair");
+    let pair = network.get(name).expect(format!("Could not find pair for {}", name).as_str());
     match direction {
         Direction::LEFT => pair.0.as_str(),
         Direction::RIGHT => pair.1.as_str(),
@@ -91,10 +132,10 @@ mod test {
 
     #[test]
     fn test_make_network() {
-        let result = make_network("a = (b, c)\ndd = (ee, ff)".lines()).unwrap();
-        let mut expected: HashMap<&str, (&str, &str)> = HashMap::new();
-        expected.insert("a", ("b", "c"));
-        expected.insert("dd", ("ee", "ff"));
+        let result = make_network(&mut "a = (b, c)\ndd = (ee, ff)".lines()).unwrap();
+        let mut expected: HashMap<String, (String, String)> = HashMap::new();
+        expected.insert("a".to_string(), ("b".to_string(), "c".to_string()));
+        expected.insert("dd".to_string(), ("ee".to_string(), "ff".to_string()));
         assert_eq!(expected, result)
     }
 }
